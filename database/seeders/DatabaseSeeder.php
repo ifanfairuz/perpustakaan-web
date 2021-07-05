c5feae495e33ada817679ae2762fbeaa951124ac
Ifan Fairuz <ifanfairuz@gmail.com>
2021-07-05 17:59:48 +0700

    [ADD] user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-        	SiteSeeder::class
+        	SiteSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
